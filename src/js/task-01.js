6831d8cb18f6c330ed5d12ff806bc44d4b9d847f
Nazar Smutnitskiy <nsmutnitskiy@swivl.com>
2024-01-15 21:42:45 +0200

    add fixes

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,8 +4,8 @@ const categoryItems = categoriesList.querySelectorAll('li.item');
 console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach((categoryItem) => {
-    const categoryName = categoryItem.querySelector('h2').textContent;
-    const categoryItemsCount = categoryItem.querySelectorAll('ul li').length;
+    const categoryName = categoryItem.firstElementChild.textContent;
+    const categoryItemsCount = categoryItem.lastElementChild.children.length;
 
     console.log(`Category: ${categoryName}`);
     console.log(`Elements: ${categoryItemsCount}`);
